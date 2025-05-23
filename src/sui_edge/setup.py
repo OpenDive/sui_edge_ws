0d1775c307b32ce51b79992e7d6f5c0382fde89b
Irvin <irvsteve@gmail.com>
2025-05-23 06:33:02 -0400

    Add cfg to define build location

--- src/sui_edge/setup.py
+++ src/sui_edge/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 from glob import glob
 
@@ -7,15 +7,16 @@ package_name = 'sui_edge'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch',
+        (os.path.join('share', package_name, 'launch'),
             glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        ('share/' + package_name + '/config',
+        (os.path.join('share', package_name, 'config'),
             glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('lib', package_name), [])  # This ensures the lib/sui_edge directory is created
     ],
     install_requires=['setuptools', 'pysui>=0.84.0'],
     zip_safe=True,
@@ -26,11 +27,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'sui_edge_bridge_node = sui_edge.bridge_node:main',
-            'sui_edge_sui_service_node = sui_edge.sui_service_node:main',
+            'bridge_node = sui_edge.bridge_node:main',
+            'sui_service_node = sui_edge.sui_service_node:main',
         ],
     },
-    scripts=[],
-    package_dir={'': '.'},
-    package_data={},
 ) 
